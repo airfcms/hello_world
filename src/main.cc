db0c15e27ce92164862e21f764439a3146100763
mavarzea <mavarzea@criticalsoftware.com>
2021-11-10 12:04:49 +0000

    Added one mroe crash

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,8 @@
 int main() {
     int x;
     std::cout << "Hello World!";
-    std::cout << "PR TEST"!;
-    std::cout << "PR TEST 2!
+    std::cout << "PR TEST 1"!;
+    std::cout << "PR TEST 2!";
+    std::cout << "PR TEST 3!
     return 0;
 }
