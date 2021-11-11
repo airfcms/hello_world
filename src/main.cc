fadc2ff799d3eb3715f17942c6d07b3a7d2b308e
mavarzea <mavarzea@criticalsoftware.com>
2021-11-11 12:17:05 +0000

    Working

--- src/main.cc
+++ src/main.cc
@@ -3,8 +3,6 @@
 int main() {
     int x;
     std::cout << "Hello World!";
-    std::cout << "PR TEST 1"!;
-    std::cout << "PR TEST 2!";
-    std::cout << "PR TEST 3!
+    std::cout << "Working";
     return 0;
 }
